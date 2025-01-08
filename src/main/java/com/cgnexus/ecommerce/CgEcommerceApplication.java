033209126e25125718323fbd46a8a94eb1691e41
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-01-08 13:26:28 +0530

    initial commit

---
+++ src/main/java/com/cgnexus/ecommerce/CgEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.cgnexus.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgEcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cgnexus/ecommerce/CgEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgnexus.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
