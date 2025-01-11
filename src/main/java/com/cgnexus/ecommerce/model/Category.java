e5b1dd6360fc7719f6863bb178d6f64a165fa403
wchamara <chamara.weerasinghe@my.sliit.lk>
2025-01-11 16:55:02 +0530

    commit message

--- src/main/java/com/cgnexus/ecommerce/model/Category.java
+++ src/main/java/com/cgnexus/ecommerce/model/Category.java
@@ -1,12 +1,20 @@
 package com.cgnexus.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "categories")
 public class Category {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
 }
--- src/main/java/com/cgnexus/ecommerce/service/CategoryServiceImpl.java
+++ src/main/java/com/cgnexus/ecommerce/service/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.cgnexus.ecommerce.service;
 
 import com.cgnexus.ecommerce.model.Category;
+import com.cgnexus.ecommerce.repositories.CategoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -9,42 +11,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
-    private final List<Category> categories = new ArrayList<>();
+
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
+
+    private final CategoryRepository categoryRepository;
 
     @Override
     public void createCategory(Category category) {
-        Long id = (long) (categories.size() + 1);
-        category.setCategoryId(id);
-        categories.add(category);
+        categoryRepository.save(category);
     }
 
     @Override
     public String deleteCategory(Long categoryId) throws ResponseStatusException {
 
-        Category category = categories.stream().filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst()
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+        if (!categoryRepository.existsById(categoryId)) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found");
+        }
 
-        categories.remove(category);
+        categoryRepository.deleteById(categoryId);
 
         return "Category with id " + categoryId + " deleted successfully";
     }
 
     @Override
     public String updateCategory(Long categoryId, Category category) throws ResponseStatusException {
-        Category categoryToUpdate = categories.stream().filter(c -> c.getCategoryId().equals(categoryId))
-                .findFirst()
+        Category categoryToUpdate = categoryRepository.findById(categoryId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
 
         if (category.getCategoryName() != null) {
             categoryToUpdate.setCategoryName(category.getCategoryName());
         }
+
+        categoryRepository.save(categoryToUpdate);
 
         return "Category with id " + categoryId + " updated successfully";
     }
